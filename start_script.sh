d3a40100b28467256ade06334d597eedb3b95354
Jihuai <wpyjihuai@gmail.com>
2025-10-17 14:25:16 +0800

    cleanup start script

--- start_script.sh
+++ start_script.sh
@@ -1,6 +1,16 @@
+# train
+python train.py \
+--config configs/melunet/bass.yaml
+
 # inference
 python inference.py \
 --config configs/melunet/bass.yaml \
 --checkpoint logs/melunet/bass/checkpoints/00260000.ckpt \
 --input_dir ../../data/MSRBench/Bass/mixture/ \
---output_dir output/melunet_00260000 
+--output_dir output/melunet/bass_00260000/
+
+# eval
+python eval_plus.py \
+--target_dir ../../data/MSRBench/Bass/target/ \
+--output_dir output/melunet/bass_00260000/ \
+--output_file output/melunet/bass_00260000.txt
